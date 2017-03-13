20f79497f7d530b7431bf3e9eb1811e740736301
nate <nshiff@users.noreply.github.com>
2017-03-12 20:09:49 -0400

    super basic frontend

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// following guide at https://css-tricks.com/gulp-for-beginners/
+
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('scss/styles.scss')
+        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', ['sass']);
+    // Other watchers
+})
